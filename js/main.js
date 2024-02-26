4bbdf960a5e8a30bb6c1e3bccd71b024e33c918b
Felix Agner <felix.agner@control.lth.se>
2024-02-26 12:48:17 +0100

    Made drawing the board work

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
-let pegs = [
-    { x: 25, y: 75, radius: 10, isSelected: false },
-    { x: 75, y: 75, radius: 10, isSelected: false },
-    { x: 125, y: 75, radius: 10, isSelected: false },
-    // Add more pegs as needed
-  ];
-  
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+import { loadBoard } from './boardManager.js';
+import { boardSketch } from './rendering.js';
 
-function drawPegs() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-  pegs.forEach(peg => {
-    ctx.beginPath();
-    ctx.arc(peg.x, peg.y, peg.radius, 0, Math.PI * 2);
-    ctx.fillStyle = peg.isSelected ? 'red' : 'blue'; // Highlight selected peg
-    ctx.fill();
-  });
-}
+// Instantiate the sketch
+let board = loadBoard();
 
-drawPegs(); // Initial drawing
+
+const sketch = boardSketch(board);
+new p5(sketch);
--- js/rendering.js
+++ js/rendering.js
@@ -6,3 +6,65 @@
  * 
  * @module renderer
  */
+
+
+// Import p5.js as a module
+import p5 from 'https://cdn.skypack.dev/p5';
+
+const boardSize = {
+    width: 800,
+    height: 800,
+    scaleFactor: 0.1
+}
+
+const colors = {
+    rhombus: 'rgba(50, 50, 150, 0.5)',
+    background: 'rgba(150, 150, 255, 0.1)'
+}
+
+// Define a sketch function
+export function boardSketch(gameboard) {
+    let diameter = 100;
+    let board = gameboard;
+    let size = {
+        x: boardSize.width,
+        y: boardSize.height
+    }
+    return (p) => {
+        p.setup = function() {
+            p.createCanvas(size.x, size.y);
+        
+          p.draw = function() {
+            p.background(colors.background);
+            // draw all the rhombs in board
+            board.rhombs.forEach(rhomb => {
+                drawRhombus(p, rhomb);
+            });
+          };
+        }
+    }
+};
+
+function scaleRhomb(rhomb) {
+    let f = (i, j) => ({
+        x: boardSize.width * (1/2 + i * boardSize.scaleFactor), 
+        y: boardSize.height * (1/2 + j * boardSize.scaleFactor), 
+    });
+    return [ 
+        f(rhomb.x_1, rhomb.y_1), 
+        f(rhomb.x_2, rhomb.y_2), 
+        f(rhomb.x_3, rhomb.y_3), 
+        f(rhomb.x_4, rhomb.y_4)
+    ];
+}
+
+function drawRhombus(p, rhomb) {
+    // draw a rhombus using x1, y1, x2, y2, x3, y3, x4, y4   
+    let coords = scaleRhomb(rhomb);
+    p.fill(colors.rhombus); // Example: semi-transparent red
+    p.beginShape();
+    coords.forEach(coord => {
+        p.vertex(coord.x, coord.y);
+    });
+    p.endShape(p.CLOSE); // Use p.CLOSE to close the shape
+}
