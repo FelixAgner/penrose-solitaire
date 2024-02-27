f94aae2db41636924572a0a4f93510a29a843747
Felix Agner <felix.agner@control.lth.se>
2024-02-27 13:39:09 +0100

    Updated line colors

--- js/config.js
+++ js/config.js
@@ -9,9 +9,9 @@ export const colors = {
     rhombus: 'rgba(50, 50, 150, 0.5)',
     background: 'rgba(150, 150, 255, 1)',
     rhombusLine: 'rgba(240, 240, 255, 0.8)',
-    moveLine: 'rgba(255, 255, 255, 0.8)',
+    moveLine: 'rgba(180, 180, 180, 0.4)',
     pegFill: 'rgba(0, 0, 255, 1)',
-    pegHole: 'rgba(0, 0, 20, 1.0)',
+    pegHole: 'rgba(40, 40, 100, 1.0)',
     pegSelected: 'rgba(240, 240, 255, 1.0)',
     pegLine: 'rgba(255, 255, 255, 0.8)',
 }
